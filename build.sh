215f538d4f0549e68c8af11ea9aa9f503690e809
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2024-05-31 13:18:36 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 make
 g++ -std=c++11 -o metrics_exporter metrics_exporter.cpp -lpthread
 docker build -t gddr6-metrics-exporter .
-docker tag gddr6-metrics-exporter jjziets/gddr6-metrics-exporter:latest
-docker push jjziets/gddr6-metrics-exporter:latest
+docker tag gddr6-metrics-exporter alkorolyov/gddr6-metrics-exporter:latest
+docker push alkorolyov/gddr6-metrics-exporter:latest
 
